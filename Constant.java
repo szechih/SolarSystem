f9bef05bed186983b2e9a02ba935f21d96b89f58
szechih <34098522+szechih@users.noreply.github.com>
2018-10-21 18:59:23 +0800

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,11 @@
+package tw.oasisworld.util;
+/**
+ * 游戏项目中用到的常量
+ * @author Administrator
+ *
+ */
+public class Constant {
+	
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 500;
+}
